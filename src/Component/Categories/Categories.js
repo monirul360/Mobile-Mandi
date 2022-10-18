355fe94eeab44ed1b4f38da128210b97060c73d9
monirul360 <monirulislam65361@gmail.com>
2022-10-18 20:21:58 +0600

    categories component design

---
+++ src/Component/Categories/Categories.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import user from './../../Photo/Article/Ellipse 12.png';
+const Categories = () => {
+    return (
+        <section>
+            <div className="Categories">
+                <div className="container">
+                    <h3 className=' pt-4 pb-4 text-center'>Our Categories</h3>
+                    <div className="Categories-items">
+                        {/* items start */}
+                        <div className="items">
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <div>
+                                    <img className='img-fluid' src={user} alt="" />
+                                </div>
+                                <div className="content ms-3">
+                                    <h4>Flowers</h4>
+                                    <p>58 Products</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* items end */}
+                        <div className="items">
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <div>
+                                    <img className='img-fluid' src={user} alt="" />
+                                </div>
+                                <div className="content ms-3">
+                                    <h4>Flowers</h4>
+                                    <p>58 Products</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* items end */}
+                        <div className="items">
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <div>
+                                    <img className='img-fluid' src={user} alt="" />
+                                </div>
+                                <div className="content ms-3">
+                                    <h4>Flowers</h4>
+                                    <p>58 Products</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* items end */}
+                        <div className="items">
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <div>
+                                    <img className='img-fluid' src={user} alt="" />
+                                </div>
+                                <div className="content ms-3">
+                                    <h4>Flowers</h4>
+                                    <p>58 Products</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* items end */}
+                        <div className="items">
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <div>
+                                    <img className='img-fluid' src={user} alt="" />
+                                </div>
+                                <div className="content ms-3">
+                                    <h4>Flowers</h4>
+                                    <p>58 Products</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* items end */}
+                        <div className="items">
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <div>
+                                    <img className='img-fluid' src={user} alt="" />
+                                </div>
+                                <div className="content ms-3">
+                                    <h4>Flowers</h4>
+                                    <p>58 Products</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* items end */}
+                        <div className="items">
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <div>
+                                    <img className='img-fluid' src={user} alt="" />
+                                </div>
+                                <div className="content ms-3">
+                                    <h4>Flowers</h4>
+                                    <p>58 Products</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* items end */}
+                        <div className="items">
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <div>
+                                    <img className='img-fluid' src={user} alt="" />
+                                </div>
+                                <div className="content ms-3">
+                                    <h4>Flowers</h4>
+                                    <p>58 Products</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* items end */}
+                        <div className="items">
+                            <div className='d-flex justify-content-center align-items-center'>
+                                <div>
+                                    <img className='img-fluid' src={user} alt="" />
+                                </div>
+                                <div className="content ms-3">
+                                    <h4>Flowers</h4>
+                                    <p>58 Products</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Categories;
--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -4,6 +4,7 @@ import hero from './../../Photo/Hero/Rectangle 2.png';
 import article from './../../Photo/Article/Rectangle 20.png';
 import location from './../../Photo/Article/Vector.png';
 import user from './../../Photo/Article/Ellipse 12.png';
+import Categories from './../../Component/Categories/Categories';
 const Home = () => {
     const post = [1, 2, 3, 4, 5, 6];
     return (
@@ -20,6 +21,56 @@ const Home = () => {
                     </div>
                     <div className="hero-img">
                         <img src={hero} alt="" />
+                    </div>
+                </div>
+            </section>
+            <Categories></Categories>
+            <section>
+                <div className="recent-product">
+                    <div className="container">
+                        <div className="popular-article">
+                            <h3 className='pb-5 pt-4 text-center'>Recent Listed Product</h3>
+                            <div className="row">
+                                {
+                                    post.map(popular =>
+
+                                        <div className="col-md-6 col-lg-4">
+                                            <div className='items'>
+                                                <div className="img p-4">
+                                                    <img className='img-fluid' src={article} alt="" />
+                                                </div>
+                                                <div className="content ps-4 ms-2">
+                                                    <h2>6,000 Rs/-</h2>
+                                                    <p>1 Bag of 300 Kgs</p>
+                                                    <p>Masoor Dal Karnataka Bag</p>
+                                                    <p>25 October 2021 - 2.30 PM</p>
+                                                </div>
+                                                <div className="footer">
+                                                    <div className='d-flex justify-content justify-content-between m-4 mt-3 pb-4'>
+                                                        <div className="user d-flex align-items-center">
+                                                            <div >
+                                                                <img className='user-img' src={user} alt="" />
+                                                            </div>
+                                                            <div className='ms-2'>
+                                                                <h4>by Jonib Smith</h4>
+                                                            </div>
+                                                        </div>
+                                                        <div className="d-flex align-items-center">
+                                                            <div className="location">
+                                                                <img src={location} alt="" />
+                                                            </div>
+                                                            <div className='text'>
+                                                                <h4 className='ms-2'>Delhi</h4>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
@@ -43,7 +94,7 @@ const Home = () => {
                                                 <p>25 October 2021 - 2.30 PM</p>
                                             </div>
                                             <div className="footer">
-                                                <div className='d-flex justify-content justify-content-between m-5 mt-3 pb-4'>
+                                                <div className='d-flex justify-content justify-content-between m-4 mt-3 pb-4'>
                                                     <div className="user d-flex align-items-center">
                                                         <div >
                                                             <img className='user-img' src={user} alt="" />
